4cf10546678f33dd590bd6df8ada3d4c24e567a8
masa3000 <masa3000ntg@icloud.com>
2024-11-21 00:09:27 +0900

    add sound maneger class

--- OurWerewolf/main/AudioPlaying.swift
+++ OurWerewolf/main/AudioPlaying.swift
@@ -8,30 +8,62 @@
 import SwiftUI
 import AVFoundation
 
+
+class AudioPlayerManager: ObservableObject {
+	var instanceForSound: AVAudioPlayer?
+
+	@objc func myInit(fileName: String) {
+		do {
+			instanceForSound = try AVAudioPlayer(data: NSDataAsset(name: fileName)!.data)
+			instanceForSound?.setVolume(0.05, fadeDuration: 0.01)
+			instanceForSound?.numberOfLoops = -1
+		} catch {
+			print("Failed to initialize AVAudioPlayer: \(error)")
+		}
+
+		NotificationCenter.default.addObserver(self, selector: #selector(playSound), name: UIApplication.willEnterForegroundNotification, object: nil)
+	}
+	
+	@objc func setSoundFile(fileName: String) {
+		do {
+			instanceForSound = try AVAudioPlayer(data: NSDataAsset(name: fileName)!.data)
+			instanceForSound?.setVolume(0.05, fadeDuration: 0.01)
+			instanceForSound?.numberOfLoops = -1
+		} catch {
+			print("Failed to initialize AVAudioPlayer: \(error)")
+		}
+	}
+
+	@objc private func playSound() {
+		instanceForSound?.play()
+	}
+
+	deinit {
+		NotificationCenter.default.removeObserver(self, name: UIApplication.willEnterForegroundNotification, object: nil)
+	}
+}
+
+
 struct AudioPlayerView: View {
+	@StateObject private var audioManager = AudioPlayerManager()
 	@EnvironmentObject var gameStatusData: GameStatusData
 	@EnvironmentObject var gameProgress: GameProgress  // to distinguish win/lose
 	@State var videoFileName: String = " "
-	@State var instanceForSound: AVAudioPlayer!
 	
-	private func playSound(){
-		instanceForSound.stop()
-		instanceForSound.currentTime = 0.0
-		instanceForSound.play()
-		instanceForSound.numberOfLoops = -1
+	private func audioInit(){
+		audioManager.myInit(fileName: videoFileName)
+		audioManager.instanceForSound?.play()
 	}
 	
-	private func continuePlayingSound(){
-		instanceForSound.stop()
-		instanceForSound.play()
-		instanceForSound.numberOfLoops = -1
+	private func playSound(){
+		audioManager.instanceForSound?.play()
 	}
 	
 	private func stopSound(){
-		instanceForSound.stop()
+		audioManager.instanceForSound?.stop()
 	}
 	
-	private func detectsFilename(){
+	private func detectAndSetAFilename(){
 		if gameStatusData.game_status == .titleScreen{
 			self.videoFileName = gameStatusData.soundTheme.titleScreen
 		}else if gameStatusData.game_status == .homeScreen{
@@ -48,40 +80,42 @@ struct AudioPlayerView: View {
 		let _ = print("file name: \(videoFileName)")
 	}
 	
+	private func setFilenameToManager(){
+		audioManager.setSoundFile(fileName: videoFileName)
+	}
+	
 	var body: some View {
 		if gameStatusData.soundMuted == false{
 			VStack{
 			}
 			.onAppear(){
-				detectsFilename()
-				do {
-					instanceForSound = try AVAudioPlayer(data: NSDataAsset(name: videoFileName)!.data)
-				} catch {
-					print("Failed to initialize AVAudioPlayer: \(error)")
-				}
-				NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: .main) { _ in
-					instanceForSound?.play()
-				}
-				instanceForSound.setVolume(0.05, fadeDuration: 0.4)
-				playSound()
+				detectAndSetAFilename()
+				audioInit()
 			}
 			.onChange(of: gameStatusData.soundMuted){ new in
 				if new == true{
 					stopSound()
 				}else{
+					stopSound()
+					detectAndSetAFilename()
+					setFilenameToManager()
 					playSound()
 				}
 			}
 			.onChange(of: gameStatusData.soundTheme){ new in
 				stopSound()
-				detectsFilename()
-				instanceForSound = try!  AVAudioPlayer(data: NSDataAsset(name: videoFileName)!.data)
-				instanceForSound.setVolume(0.35, fadeDuration: 0.4)
-				continuePlayingSound()
+				detectAndSetAFilename()
+				setFilenameToManager()
+				playSound()
+			}
+			.onChange(of: gameStatusData.game_status){ new in
+				stopSound()
+				detectAndSetAFilename()
+				setFilenameToManager()
+				playSound()
 			}
 			.onDisappear {
 				stopSound()
-				NotificationCenter.default.removeObserver(self, name: UIApplication.willEnterForegroundNotification, object: nil)
 			}
 		}
 	}
