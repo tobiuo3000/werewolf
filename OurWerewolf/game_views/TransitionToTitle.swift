7af25c64b804eed700f75e3e8b90476ebfac3109
masa3000 <masa3000ntg@icloud.com>
2024-12-09 19:06:51 +0900

    add TransitionToTitle file

--- OurWerewolf/game_views/TransitionToTitle.swift
+++ OurWerewolf/game_views/TransitionToTitle.swift
@@ -8,5 +8,19 @@
 import SwiftUI
 
 
+struct toTitleView: View{
+	@EnvironmentObject var gameStatusData: GameStatusData
+	private let delay: Double = 1.4
+	
+	var body: some View{
+		TransitionLoopBackGround(offset: CGSize(width: 0,
+												height: 0))
+		.onAppear(){
+			DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+				gameStatusData.game_status = .titleScreen
+			}
+		}
+	}
+}
 
 
